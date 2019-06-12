66993f0ab4fb3db4785fa84c9eaf61d891d49325
Oskar Grunning <oskargrunning@gmail.com>
2019-06-12 21:43:06 +0200

    feat: view random snippet
    This closes: #224.

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,5 @@
 import { anyPass, compose, endsWith, is, replace, startsWith } from 'ramda'
 
-export const startsOrEndsWith = (val, query) =>
-  anyPass ([startsWith, endsWith]) (query, val)
 export const enforceSingleNewLine = x =>
   is (String, x)
     ? compose (
@@ -9,3 +7,9 @@ export const enforceSingleNewLine = x =>
         replace (/[\r\n]*$/, '')
       ) (x)
     : x
+
+export const randomNumberInRange = (min, max) =>
+  Math.floor (Math.random () * (max - min + 1)) + min
+
+export const startsOrEndsWith = (val, query) =>
+  anyPass ([startsWith, endsWith]) (query, val)
